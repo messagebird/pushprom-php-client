44943a40412b68be303155c797708f01b141b5f9
Jagjeet Dhaliwal <jagjeet@messagebird.com>
2018-11-26 14:15:58 +0100

    Add ability to push buckets using Histograms

--- Histogram.php
+++ Histogram.php
@@ -4,13 +4,19 @@ namespace pushprom;
 
 class Histogram extends Metric
 {
-    function observe($value)
+    function observe($value, $buckets = [])
     {
+        $attrs = [
+            'value' => $value,
+            'method' => 'observe',
+        ];
+
+        if (!empty($buckets)) {
+            $attrs['buckets'] = $buckets;
+        }
+
         return $this->pushDelta(
-            [
-                "value"  => $value,
-                "method" => "observe",
-            ]
+            $attrs
         );
     }
 }
