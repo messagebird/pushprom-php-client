6b8a9ec480523b07e0eaf3294b0e481428d97ff9
Jagjeet Dhaliwal <jagjeet@messagebird.com>
2018-11-26 14:46:09 +0100

    Allow empty buckets

--- Histogram.php
+++ Histogram.php
@@ -6,17 +6,12 @@ class Histogram extends Metric
 {
     function observe($value, $buckets = [])
     {
-        $attrs = [
-            'value' => $value,
-            'method' => 'observe',
-        ];
-
-        if (!empty($buckets)) {
-            $attrs['buckets'] = $buckets;
-        }
-
         return $this->pushDelta(
-            $attrs
+            [
+                'value' => $value,
+                'method' => 'observe',
+                'buckets' => $buckets,
+            ]
         );
     }
 }
